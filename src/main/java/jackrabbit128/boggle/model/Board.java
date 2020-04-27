c024e7587dca98c1fe87c61ae8bd8a3bc4f3091a
jackrabbit128 <jackrabbit128@gmail.com>
2020-04-27 22:36:53 +0200

    Make sure Board shuffle is predictable for a given seed. Again.
    Added a unit test as well.

--- src/main/java/jackrabbit128/boggle/model/Board.java
+++ src/main/java/jackrabbit128/boggle/model/Board.java
@@ -7,28 +7,29 @@ import java.util.stream.IntStream;
 public final class Board {
   private final int _width;
   private final int _height;
+  private final List<Die> _sortedDice;
 
-  List<Die> _dice;
+  private List<Die> _dice;
 
   public Board(int width, int height, Collection<Die> dice) {
     if (width * height != dice.size()) {
       throw new IllegalArgumentException("expected " + width * height + " dice, but got " + dice.size());
     }
 
-    var sortedDice = new ArrayList<>(dice);
-    sortedDice.sort(Comparator.comparing(d -> String.join(",", d.options())));
+    _sortedDice = new ArrayList<>(dice);
+    _sortedDice.sort(Comparator.comparing(d -> String.join(",", d.options())));
 
     _width = width;
     _height = height;
-    _dice = List.copyOf(sortedDice);
+    _dice = List.copyOf(_sortedDice);
   }
 
   public void shuffle(Random random) {
-    var shuffledDice = new ArrayList<Die>(_dice.size());
-    var indices = IntStream.range(0, _dice.size()).boxed().collect(Collectors.toList());
+    var shuffledDice = new ArrayList<Die>(_sortedDice.size());
+    var indices = IntStream.range(0, _sortedDice.size()).boxed().collect(Collectors.toList());
     while (!indices.isEmpty()) {
       var index = indices.remove(random.nextInt(indices.size()));
-      shuffledDice.add(_dice.get(index));
+      shuffledDice.add(_sortedDice.get(index));
     }
     for (Die die : shuffledDice) {
       die.roll(random);
---
+++ src/test/java/jackrabbit128/boggle/model/BoardTest.java
@@ -0,0 +1,56 @@
+package jackrabbit128.boggle.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class BoardTest {
+  private Board _board;
+  private Random _random;
+
+  @BeforeEach
+  public void setUp() {
+    _board = new Board(2, 2, Locale.getDefault(), List.of(
+        new Die(List.of("A", "B", "C", "D")),
+        new Die(List.of("E", "F", "G", "H")),
+        new Die(List.of("I", "J", "K", "L")),
+        new Die(List.of("M", "N", "O", "P"))
+    ));
+
+    _random = new Random();
+  }
+
+  @Test
+  public void shuffleWithSameSeedYieldsSameResult() {
+    long seed = 1;
+    Supplier<String> shuffleAndReadBoard = () -> {
+      _random.setSeed(seed);
+      _board.shuffle(_random);
+      return _board.toString();
+    };
+
+    var one = shuffleAndReadBoard.get();
+    var two = shuffleAndReadBoard.get();
+    assertEquals(one, two);
+  }
+
+  @Test
+  public void shuffleWithoutSeedingYieldsDifferentResult() {
+    _random.setSeed(1);
+    Supplier<String> shuffleAndReadBoard = () -> {
+      _board.shuffle(_random);
+      return _board.toString();
+    };
+
+    var one = shuffleAndReadBoard.get();
+    var two = shuffleAndReadBoard.get();
+    assertNotEquals(one, two);
+  }
+}
